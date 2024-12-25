4ad9a172ca8efd43ebd6399944f5520535a1caf0
Adam Szerszenowicz <70147389+Pogromca-SCP@users.noreply.github.com>
2024-12-25 23:57:36 +0100

    Added benchmark project

---
+++ SLCommandScript.Benchmark/Benchmark.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using SLCommandScript.Core;
+
+namespace SLCommandScript.Benchmark;
+
+public static class Runner
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<SLCSBenchmark>();
+    }
+}
+
+[MemoryDiagnoser]
+public class SLCSBenchmark
+{
+    public const string Script = @"
+    bc 1 Hello World
+";
+
+    [Benchmark]
+    public void BenchmarkScript()
+    {
+        _ = ScriptUtils.Execute(Script, new(["benchmark"], 1, 0), null);
+    }
+}
