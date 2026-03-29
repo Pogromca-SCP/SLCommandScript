055cc81f60b16d17b9dd7c9a96557bd44af70b47
Adam Szerszenowicz <70147389+Pogromca-SCP@users.noreply.github.com>
2026-03-29 17:20:40 +0200

    Readded copyright year

--- SLCommandScript.FileScriptsLoader/Properties/AssemblyInfo.cs
+++ SLCommandScript.FileScriptsLoader/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 [assembly: AssemblyTitle(FileScriptsLoader.ProjectName)]
 [assembly: AssemblyDescription("Filesystem Scripts Loader implementation for SLCommandScript.")]
 [assembly: AssemblyProduct("SLCommandScript")]
-[assembly: AssemblyCopyright("Copyright © " + FileScriptsLoader.ProjectAuthor)]
+[assembly: AssemblyCopyright("Copyright © 2026 " + FileScriptsLoader.ProjectAuthor)]
 [assembly: AssemblyVersion(FileScriptsLoader.ProjectVersion)]
 [assembly: AssemblyFileVersion(FileScriptsLoader.ProjectVersion)]
 [assembly: AssemblyInformationalVersion(FileScriptsLoader.ProjectVersion)]
